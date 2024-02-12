65d29ec19edcf5c81a9a64cc2c7c7bc2e2978508
Gamze Şirin <gamze1sirin@gmail.com>
2024-02-12 17:00:38 +0300

    updated.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import Campaings from '../components/campaings/Campaings'
 import Cards from '../components/cards/Cards'
-import Categories from '../components/Categories/Categories'
+import Categories from '../components/categories/Categories'
 import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import HeroSection from '../components/herosection/HeroSection'
